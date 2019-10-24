2e132634d8ffc4011bf16d471585a882813971ce
MrLaike <laikelst@gmail.com>
2019-10-24 23:02:20 +0000

    completed and remove todo item

--- src/assets/js/components/ToDo.vue
+++ src/assets/js/components/ToDo.vue
@@ -1,27 +1,84 @@
 
 <template>
-	<div class="todo">
-		<ul>
-			<h2>TODO</h2>
-			<li v-if="todoList" v-for="listItem in todoList" :item="listItem"><span class="checkbox"></span>{{listItem}}<span class="delete">x</span></li>
+	<div class="todo" >
+		<h2>TODO</h2>
+		<ul v-show="todoList.length">
+			<li v-for="todo in todoList" 
+				:key="todo.id"
+				:class="{completed: todo.completed}">
+				<input type="checkbox" name="checkbox" @change="isChecked(todo)" class="checkbox"><span class="custom-checkbox"></span>
+				<label>{{todo.title}}</label>
+				<button class="delete" @click="removeTodo(todo)">x</button></li>
 		</ul>
-		<input type="text" v-model="item" @keyup.enter="addItem" class="todo-input" autocomplete="false" placeholder="Add your todo">
+		<input type="text" v-model="item" @keyup.enter="addTodo" class="todo-input" autocomplete="false" placeholder="Add your todo">
 	</div>
 </template>
 <script >
+	var LOCALSTORAGE_KEY = 'todo-module'
+
+	var todoStorage = {
+		fetch: function(){
+			var todos = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY) || '[]')
+
+			todos.forEach(function(todo, index){
+				todo.id = index
+			})
+			todoStorage.uid = todos.length
+
+			return todos
+		},
+		save: function(todos){
+			localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(todos))
+		}
+
+	}
+
 	export default {
 		name: 'todo',
-		props:['todoList'],
-		data: {
-			item: ''
-		},
-		methods:{
-			addItem: function(){
-				this.todoList.unshift(this.item)
-				this.item = ''
+		data: function(){
+			return {
+				item: '',
+				todoList: todoStorage.fetch()
 			}
+			
+		},
+		watch: {
+			todoList: {
+				handler: function (todoList) {
+					todoStorage.save(todoList)
+				},
+				deep: true
+			}
+		},
 
-		}
+
+
+		methods: {
+			addTodo: function(){
+				this.todoList = todoStorage.fetch(),
+
+				this.todoList.push({
+					id: todoStorage.uid++,
+					title: this.item,
+					completed: false
+
+				})
+
+				//todoStorage.save(this.todoList)
+				
+				this.item = ''
+			},
+
+			removeTodo: function(todo){
+				this.todoList.splice(this.todoList.indexOf(todo), 1)
+				//todoStorage.save(this.todoList)
+			},
+
+			isChecked: function(todo){
+				todo.completed = !todo.completed 
+			}
+		},
+		
 	};
 </script>
 <style scoped>

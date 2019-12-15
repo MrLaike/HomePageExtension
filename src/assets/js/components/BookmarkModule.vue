464434348e620a067fcfe86757d80ddeb06814ef
MrLaike <laikelst@gmail.com>
2019-12-15 17:17:50 +0000

    Update npm package serialize-javascript >=1.2.1

--- src/assets/js/components/BookmarkModule.vue
+++ src/assets/js/components/BookmarkModule.vue
@@ -1,5 +1,21 @@
 <template>
 	<div class="bookmark">
+		<div class="filter">
+			<div class="sort-block">
+				<p>Sort by: </p>
+				<select>
+					<option value="date">date</option>
+					<option value="views">views</option>
+					<option value="title">title</option>
+
+				</select>
+			</div>
+			<div class="hash-tag">
+				<a class="tags active" href="#php">#php</a>
+				<a class="tags" href="#js">#js</a>
+				<a class="tags" href="#ai">#ai</a>
+			</div>
+		</div>
 		<ul class="list-wrapper">
 			<tree-item v-for="node in bookmarks" :nodes="node.children" :title="node.title" :url="node.url"></tree-item>
 		</ul>
